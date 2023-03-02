15f2850ca54c6569f37a9cf4545dd2714b174764
Alexey Tatyanenko <57017816+alexdtat@users.noreply.github.com>
2023-03-02 13:08:30 +0300

    Update homework/hw1/Task5/Program.fs
    Constructor style fix
    
    Co-authored-by: Lozov Peter <not_kill@mail.ru>

--- homework/hw1/Task5/Program.fs
+++ homework/hw1/Task5/Program.fs
@@ -4,7 +4,7 @@ let findFirstEntrance list element =
     let rec recFindFirstEntrance list element counter =
         match list with
         | [] -> None
-        | head :: _ when head = element  -> Some(counter)
+        | head :: _ when head = element -> Some counter
         | _ -> recFindFirstEntrance list.Tail element (counter + 1)
     recFindFirstEntrance list element 0
 
