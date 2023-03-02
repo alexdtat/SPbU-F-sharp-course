e6484e9325b793cd8b70391ea6b017665f4f24f5
Alexey Tatyanenko <57017816+alexdtat@users.noreply.github.com>
2023-03-02 13:09:30 +0300

    Update homework/hw1/Task5/Program.fs
    List pattern matching style fix
    
    Co-authored-by: Lozov Peter <not_kill@mail.ru>

--- homework/hw1/Task5/Program.fs
+++ homework/hw1/Task5/Program.fs
@@ -5,7 +5,7 @@ let findFirstEntrance list element =
         match list with
         | [] -> None
         | head :: _ when head = element -> Some counter
-        | _ -> recFindFirstEntrance list.Tail element (counter + 1)
+        | _ :: tail -> recFindFirstEntrance tail element (counter + 1)
     recFindFirstEntrance list element 0
 
 printfn $"{findFirstEntrance [0;1;2;3] 3}"
