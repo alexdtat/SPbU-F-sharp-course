aa10198cd0dcadd8fd21de3b6f2dfacee4e41fcb
Alexey <alexdtat@gmail.com>
2023-03-28 18:40:08 +0300

    2nd homework tasks implementations were added.

---
+++ homework/hw2/hw2/hw2/Task2.fs
@@ -0,0 +1,23 @@
+﻿module hw2.treeMap
+
+module Tree =
+    type BinTree<'a> =
+        | Tree of 'a * BinTree<'a> * BinTree<'a>
+        | Empty
+
+    let map transform tree =
+        let rec mapRec tree =
+            match tree with
+            | Tree (value, l, r) -> Tree((transform value), (mapRec l), (mapRec r))
+            | Empty -> Empty
+
+        mapRec tree
+
+    let mapCPS transform tree =
+        let rec mapRecCPS tree continuation =
+            match tree with
+            | Tree (value, leftTree, rightTree) ->
+                mapRecCPS leftTree (fun l -> mapRecCPS rightTree (fun r -> continuation (Tree(transform value, l, r))))
+            | Empty -> continuation Empty
+
+        mapRecCPS tree id
---
+++ homework/hw2/hw2/hw2/Task4.fs
@@ -0,0 +1,19 @@
+﻿module hw2.primes
+
+let primes () =
+    let isPrimeBig n =
+        let rec recMultipliers current upperBound =
+            match current with
+            | _ when current > bigint 1u && current <= upperBound && n % current = bigint 0u -> false
+            | _ when current <= upperBound -> recMultipliers (current + bigint 1u) upperBound
+            | _ -> true
+
+        (n > bigint 1u)
+        && recMultipliers (bigint 2u) ((float >> sqrt >> floor >> bigint) n)
+    
+    Seq.unfold (fun bigNumber ->
+        match bigNumber with
+        | _ when isPrimeBig bigNumber && bigNumber = bigint 2u -> Some(bigNumber, bigNumber + bigint 1u)
+        | _ when isPrimeBig bigNumber -> Some(bigNumber, bigNumber + bigint 2u)
+        | _ -> Some(bigint -1, bigNumber + bigint 1u)) (bigint 2u)
+    |> Seq.filter (fun bigNumber -> bigNumber > bigint 0u)
