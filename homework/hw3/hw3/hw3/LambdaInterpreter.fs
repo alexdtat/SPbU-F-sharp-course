c37b28ca0f2268484ff8953acfec71d0a415c2b2
Alexey <alexdtat@gmail.com>
2023-04-07 02:10:06 +0300

    Lambda interpreter implementation has been added with tests.

---
+++ homework/hw3/hw3/hw3/LambdaInterpreter.fs
@@ -0,0 +1,101 @@
+﻿open System
+
+module LambdaInterpreter =
+    type Variable = string
+
+    type LambdaTerm =
+        | Var of Variable
+        | Application of LambdaTerm * LambdaTerm
+        | LambdaAbstraction of Variable * LambdaTerm
+
+    let freeVariables term =
+        let rec freeVariablesCPS term continuation =
+            match term with
+            | Var name -> continuation (Set.singleton name)
+            | Application (left, right) ->
+                freeVariablesCPS left (fun l -> freeVariablesCPS right (fun r -> continuation (Set.union l r)))
+            | LambdaAbstraction (name, term) -> freeVariablesCPS term (fun t -> continuation (Set.remove name t))
+
+        freeVariablesCPS term id
+
+    let boundVariables term =
+        let rec boundVariablesCPS term continuation =
+            match term with
+            | Var _ -> continuation Set.empty
+            | Application (left, right) ->
+                boundVariablesCPS left (fun l -> boundVariablesCPS right (fun r -> continuation (Set.union l r)))
+            | LambdaAbstraction (name, term) -> boundVariablesCPS term (fun t -> continuation (Set.add name t))
+
+        boundVariablesCPS term id
+
+    let generateUniqueName usedVariables =
+        let max: string = Set.maxElement usedVariables
+        let last = Seq.last max
+
+        match Char.IsDigit last with
+        | true ->
+            max[0 .. max.Length - 2]
+            + (last |> Char.GetNumericValue |> int |> (+) 1 |> string)
+        | false -> max[0 .. max.Length - 1] + "0"
+
+    let substitute var term subTerm =
+        let needRename lVariable variable term substitutedTerm =
+            not (
+                Set.contains lVariable (freeVariables substitutedTerm)
+                && (Set.contains variable (freeVariables term))
+            )
+
+        let rec substituteCPS var term subTerm continuation =
+            match term with
+            | Var name when name = var -> continuation subTerm
+            | Var _ -> continuation term
+            | Application (left, right) ->
+                substituteCPS var left subTerm (fun l ->
+                    substituteCPS var right subTerm (fun r -> continuation (Application(l, r))))
+            | LambdaAbstraction (lVar, body) when lVar = var -> continuation (LambdaAbstraction(lVar, body))
+            | LambdaAbstraction (lVar, body) when needRename lVar var term subTerm ->
+                substituteCPS var body subTerm (fun b -> continuation (LambdaAbstraction(lVar, b)))
+
+            | LambdaAbstraction (lVar, body) ->
+                let usedVariables = (freeVariables body, freeVariables term) ||> Set.union
+                let newLVar = Variable(generateUniqueName usedVariables)
+
+                substituteCPS lVar body (Var newLVar) (fun t ->
+                    substituteCPS newLVar t subTerm (fun b -> continuation (LambdaAbstraction(newLVar, b))))
+
+        substituteCPS var term subTerm id
+
+    let alphaConversion newVariable term =
+        match term with
+        | Var variable -> Var variable
+        | Application (l, r) -> Application(l, r)
+        | LambdaAbstraction (lVar, body) when Set.contains newVariable (freeVariables body) ->
+            LambdaAbstraction(lVar, body)
+        | LambdaAbstraction (lVar, body) -> LambdaAbstraction(newVariable, substitute lVar body (Var newVariable))
+
+    let betaReduction isRecursive term =
+        let rec reduce isRecursive term =
+            match term with
+            | Var variable -> Var variable
+            | Application (LambdaAbstraction (lVar, body), substitutedTerm) ->
+                match isRecursive with
+                | false -> substitute lVar body substitutedTerm
+                | true -> reduce isRecursive (substitute lVar body substitutedTerm)
+            | Application (l, r) -> Application(l, r)
+            | LambdaAbstraction (lVar, body) -> LambdaAbstraction(lVar, body)
+
+        reduce isRecursive term
+
+    let eval term = betaReduction true term
+
+    let etaReduction term =
+        let rec reduce term =
+            match term with
+            | Var variable -> Var variable
+            | Application (l, r) -> Application(l, r)
+            | LambdaAbstraction (lVar, body) ->
+                match body with
+                | Application (l, r) when r = Var lVar -> reduce l
+                | _ -> LambdaAbstraction(lVar, body)
+
+        reduce term
---
+++ homework/hw3/hw3/hw3Tests/LambdaInterpreterTests.fs
@@ -0,0 +1,111 @@
+module hw3Tests
+
+open FsCheck
+open FsUnit
+open LambdaInterpreter.LambdaInterpreter
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () = ()
+
+[<Test>]
+let YCombinatorTest () =
+    let YCombinatorEquality f =
+        let YCombinator =
+            LambdaAbstraction(
+                Variable "f",
+                let appTerm =
+                    LambdaAbstraction(Variable "x", Application(Var "f", Application(Var "x", Var "x")))
+
+                Application(appTerm, appTerm)
+            )
+
+        let newVar = Variable (generateUniqueName (Set.add (Variable "x") (freeVariables f)))
+        let alphaConverted = alphaConversion newVar (Application(YCombinator, f))
+        (Application(f, betaReduction false alphaConverted) = betaReduction
+                                                                                false
+                                                                                (betaReduction
+                                                                                    false
+                                                                                    alphaConverted))
+
+    Check.QuickThrowOnFailure YCombinatorEquality
+
+[<Test>]
+let simpleTest1 () =
+    let term =
+        Application(
+            LambdaAbstraction("z", Var "y"),
+            let appTerm =
+                LambdaAbstraction("z", Application(Var "z", Application(Var "z", Var "z")))
+
+            Application(appTerm, appTerm)
+        )
+
+    let expectedTerm = Var "y"
+    let expectedFree = Set.singleton (Variable "y")
+    let expectedBound = Set.singleton (Variable "z")
+
+    (eval term, expectedTerm) ||> should equal
+    (freeVariables term, expectedFree) ||> should equal
+    (boundVariables term, expectedBound) ||> should equal
+
+[<Test>]
+let simpleTest2 () =
+    let term = Application(LambdaAbstraction("x", Var "x"), Var "z")
+
+    let expectedTerm = Var "z"
+    let expectedFree = Set.singleton (Variable "z")
+    let expectedBound = Set.singleton (Variable "x")
+
+    (eval term, expectedTerm) ||> should equal
+    (freeVariables term, expectedFree) ||> should equal
+    (boundVariables term, expectedBound) ||> should equal
+
+[<Test>]
+let simpleTest3 () =
+    let term = Application(LambdaAbstraction("x", Var "z"), Var "x")
+
+    let expectedTerm = Var "z"
+    let expectedFree = Set.singleton(Variable "z").Add(Variable "x")
+    let expectedBound = Set.singleton (Variable "x")
+    (eval term, expectedTerm) ||> should equal
+    (freeVariables term, expectedFree) ||> should equal
+    (boundVariables term, expectedBound) ||> should equal
+
+[<Test>]
+let etaReductionTest () =
+    let term =
+        LambdaAbstraction(
+            Variable "x",
+            Application(LambdaAbstraction(Variable "y", Application(Var "z", Var "y")), Var "x")
+        )
+
+    let expectedTerm = Var "z"
+    let expectedFree = Set.singleton (Variable "z")
+    let expectedBound = Set.singleton(Variable "x").Add(Variable "y")
+    (etaReduction term, expectedTerm) ||> should equal
+    (freeVariables term, expectedFree) ||> should equal
+    (boundVariables term, expectedBound) ||> should equal
+
+[<Test>]
+let alphaConversionTest () =
+    let converted =
+        alphaConversion
+            "a"
+            (LambdaAbstraction(
+                Variable "x",
+                Application(LambdaAbstraction(Variable "y", Application(Var "z", Var "y")), Var "x")
+            ))
+
+    let expectedTerm =
+        LambdaAbstraction(
+            Variable "a",
+            Application(LambdaAbstraction(Variable "y", Application(Var "z", Var "y")), Var "a")
+        )
+
+    let expectedFree = Set.singleton (Variable "z")
+    let expectedBound = Set.singleton(Variable "a").Add(Variable "y")
+
+    (converted, expectedTerm) ||> should equal
+    (freeVariables converted, expectedFree) ||> should equal
+    (boundVariables converted, expectedBound) ||> should equal
