0f4c39c5d7b29faabdc4cdd892f2ad23b56c17d7
Alexey Tatyanenko <57017816+alexdtat@users.noreply.github.com>
2023-04-14 19:52:27 +0300

    Update homework/hw2/hw2/hw2/Task3.fs
    Redundant braces deletion.
    
    Co-authored-by: Lozov Peter <not_kill@mail.ru>

--- homework/hw2/hw2/hw2/Task3.fs
+++ homework/hw2/hw2/hw2/Task3.fs
@@ -10,7 +10,7 @@ module ArithmeticTree =
     let eval arithmeticExpression =
         let rec evalCPS arithmeticExpression continuation =
             match arithmeticExpression with
-                | Number num -> continuation(num)
+                | Number num -> continuation num
                 | Plus(left, right) -> evalCPS left (fun l -> evalCPS right (fun r -> continuation(l + r)))
                 | Multiplication(left, right) -> evalCPS left (fun l -> evalCPS right (fun r -> continuation(l * r)))
                 | Division(left, right) -> evalCPS left (fun l -> evalCPS right (fun r -> continuation(l / r)))
