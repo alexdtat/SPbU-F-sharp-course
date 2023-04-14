1a676aa2eb76739edab9d450540350ef59770e0c
Alexey <alexdtat@gmail.com>
2023-04-14 21:03:59 +0300

    primes() function fix.

--- homework/hw2/hw2/hw2/Task4.fs
+++ homework/hw2/hw2/hw2/Task4.fs
@@ -4,16 +4,20 @@ let primes () =
     let isPrimeBig n =
         let rec recMultipliers current upperBound =
             match current with
-            | _ when current > bigint 1u && current <= upperBound && n % current = bigint 0u -> false
-            | _ when current <= upperBound -> recMultipliers (current + bigint 1u) upperBound
+            | _ when current <= upperBound ->
+                match current with
+                | _ when current > bigint 1u && n % current = bigint 0u -> false
+                | _ -> recMultipliers (current + bigint 1u) upperBound
             | _ -> true
 
         (n > bigint 1u)
         && recMultipliers (bigint 2u) ((float >> sqrt >> floor >> bigint) n)
-    
-    Seq.unfold (fun bigNumber ->
-        match bigNumber with
-        | _ when isPrimeBig bigNumber && bigNumber = bigint 2u -> Some(bigNumber, bigNumber + bigint 1u)
-        | _ when isPrimeBig bigNumber -> Some(bigNumber, bigNumber + bigint 2u)
-        | _ -> Some(bigint -1, bigNumber + bigint 1u)) (bigint 2u)
-    |> Seq.filter (fun bigNumber -> bigNumber > bigint 0u)
+
+    Seq.unfold
+        (fun bigNumber ->
+            match bigNumber with
+            | _ when bigNumber = bigint 2u -> Some(Some bigNumber, bigNumber + bigint 1u)
+            | _ when isPrimeBig bigNumber -> Some(Some bigNumber, bigNumber + bigint 2u)
+            | _ -> Some(None, bigNumber + bigint 1u))
+        (bigint 2u)
+    |> Seq.choose id
