1b29e953a7ca02e875a6d333625760ae25e806c0
Alexey <alexdtat@gmail.com>
2023-04-28 13:04:29 +0300

    Implementation of tasks 1-2 has been added.

---
+++ classwork/Test/Test/Task1.fs
@@ -0,0 +1,22 @@
+﻿module Test.Supermap
+
+let supermap functions list =
+    let revFunctions = functions |> List.rev
+    let revArguments = list |> List.rev
+
+    let rec iterate list acc =
+        match list with
+        | [] -> acc
+        | h :: t ->
+            let rec eval functions acc =
+                match functions with
+                | [] -> acc
+                | fh :: ft -> eval ft ((fh h) :: acc)
+
+            eval revFunctions acc |> iterate t
+
+    iterate revArguments []
+    
+let twice number = number * 2
+let minusTwice number = number * -2
+([(*) 2; (*) -2], [1; 2; 3]) ||> supermap |> List.iter (fun el -> printf $"%A{el} ")
---
+++ classwork/Test/Test/Task2.fs
@@ -0,0 +1,30 @@
+﻿module Test.Rhombus
+
+let rhombus side =
+    let rec getLine n =
+        match n with
+        | _ when n > 2u * side - 2u -> ()
+        | _ ->
+            let rec getSymbol pos =
+                let isStar pos =
+                    pos = side - n - 1u
+                    || pos = side + n - 1u
+                    || (n >= side && pos = n - side + 1u)
+                    || (n >= side && pos = 3u * side - 3u - n)
+
+                match pos with
+                | _ when pos = 2u * side - 1u ->
+                    printf "\n"
+                    getLine (n + 1u)
+                | _ when isStar pos ->
+                    printf "*"
+                    getSymbol (pos + 1u)
+                | _ ->
+                    printf " "
+                    getSymbol (pos + 1u)
+
+            getSymbol 0u
+
+    getLine 0u
+
+rhombus 8u
