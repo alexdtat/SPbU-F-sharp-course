53abbcfca5fe797a3da152c6b2a29649ae207c55
Alexey <alexdtat@gmail.com>
2023-05-01 23:26:19 +0300

    Implementations of tasks 1-3 with tests were added.

---
+++ homework/hw4/hw4/hw4Tasks1-2/Task2.fs
@@ -0,0 +1,11 @@
+﻿module hw4.PointFree
+
+let step0 x l = List.map (fun y -> y * x) l
+let step1 x = List.map (fun y -> y * x)
+let step2 x = List.map (fun y -> (*) y x)
+let step3 x = List.map (fun y -> (*) x y) // Multiplication is commutative for ints.
+let step4 x = List.map ((*) x)
+let step5 x = List.map (x |> (*))
+let step6 x = x |> (*) |> List.map
+let step7 = (*) >> List.map // Not initialized in FsCheck tests.
+let step7Wrapped () = (*) >> List.map
