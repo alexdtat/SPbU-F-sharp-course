a761fb8e3f758ab24bd0d10c136eb9f07bcf4a18
Alexey <alexdtat@gmail.com>
2023-05-05 13:45:12 +0300

    Implementation of Task 3 has been added with tests.

---
+++ classwork/Retest/Retest/Task3.fs
@@ -0,0 +1,41 @@
+﻿module Retest.Moves
+
+type OptionBuilder() =
+    member _.Bind(v, f) = Option.bind f v
+    member _.Return v = Some v
+    member _.Zero() = None
+
+let opt = OptionBuilder()
+
+type movement =
+    | Left of int
+    | Right of int
+    | Top of int
+    | Bottom of int
+
+let evalMoves startCoord movements =
+    let rec evalMovesRec coord movements =
+        let opt = OptionBuilder() // this instance is needed for tests
+        let newCoord (x, y) move =
+            opt {
+                let resCoord =
+                    match move with
+                    | Left step -> (x - step, y)
+                    | Right step -> (x + step, y)
+                    | Top step -> (x, y + step)
+                    | Bottom step -> (x, y - step)
+
+                if (fst resCoord >= 0 && snd resCoord >= 0) then
+                    return resCoord
+            }
+
+        opt {
+            match movements with
+            | [] -> return coord
+            | h :: t ->
+                let! coord = newCoord coord h
+                let! resCoord = evalMovesRec coord t
+                return resCoord
+        }
+
+    (startCoord, movements) ||> evalMovesRec
